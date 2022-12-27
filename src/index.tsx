4770ab97c616bfb9570504401a2a96c1c69e93e1
Mikko Suniala <mikko.suniala@iki.fi>
2022-12-27 20:39:22 +0200

    Prevent accidental zoom caused by tapping the button

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById('root') as HTMLElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Prevent accidental zooms while tapping by disabling double click events.
+document.addEventListener(
+    'dblclick',
+    (event) => {
+        event.preventDefault();
+    },
+    { passive: false }
+);
